210accc0fb8e13edea65905a804f1c06baddb46b
gilesv <gilesv@protonmail.com>
2020-11-03 11:13:51 -0300

    Fix test cases

--- measuring/src/index.js
+++ measuring/src/index.js
@@ -10,19 +10,21 @@ const App = {
     name: 'reactron',
     url: 'https://gilesv.github.io/todomvc-reactron',
   },
+  dev: {
+    name: 'dev',
+    url: 'http://localhost:8080',
+  },
 };
 
 async function main() {
   let { app, scenario, todos, screen, rounds } = yargs
-    .option('app', { choices: [App.react.name, App.reactron.name], type: 'string' })
+    .option('app', { choices: [App.react.name, App.reactron.name, App.dev.name], type: 'string' })
     .option('scenario', { alias: 's', choices: ['a', 'b', 'c', 'd', 'e'] })
     .option('todos', { default: 10, type: 'number' })
     .option('screen', { type: 'boolean', default: false })
     .option('rounds', { default: 1, type: 'number' }).argv;
 
-  let browser = await puppeteer.launch({
-    headless: true,
-  });
+  let browser = await puppeteer.launch({ headless: true });
 
   switch (scenario) {
     case 'a':
@@ -32,22 +34,22 @@ async function main() {
       break;
     case 'b':
       for (let i = 0; i < rounds; i++) {
-        scenarioB(browser, App[app], todos, i+1, screen);
+        await scenarioB(browser, App[app], todos, i+1, screen);
       }
       break;
     case 'c':
       for (let i = 0; i < rounds; i++) {
-        scenarioC(browser, App[app], todos, i+1, screen);
+        await scenarioC(browser, App[app], todos, i+1, screen);
       }
       break;
     case 'd':
       for (let i = 0; i < rounds; i++) {
-        scenarioD(browser, App[app], todos, i+1, screen);
+        await scenarioD(browser, App[app], todos, i+1, screen);
       }
       break;
     case 'e':
       for (let i = 0; i < rounds; i++) {
-        scenarioE(browser, App[app], todos, i+1, screen);
+        await scenarioE(browser, App[app], todos, i+1, screen);
       }
       break;
   }
@@ -69,7 +71,7 @@ async function scenarioA(browser, app, todos, round = 1, screenshots = false) {
   let page = await browser.newPage();
 
   await page.tracing.start({ path: resultsPath('A', app, todos, round), screenshots });
-
+  Pasta sem nome
   await page.goto(`${app.url}?todos=${todos}`);
   await page.waitForSelector('input.new-todo');
 
@@ -79,12 +81,16 @@ async function scenarioA(browser, app, todos, round = 1, screenshots = false) {
   console.log(`#${round} Done: Sc#A for ${app.name} with ${todos} todos`);
 }
 
-async function scenarioB(page, app, todos, round = 1, screenshots = false) {
-  console.log(`\nRunning Sc#B for ${app.name} with ${todos} todos...`);
+async function scenarioB(browser, app, todos, round = 1, screenshots = false) {
+  console.log(`\n#${round} Running Sc#B for ${app.name} with ${todos} todos...`);
+
+  let page = await browser.newPage();
 
   await page.tracing.start({ path: resultsPath('B', app, todos, round), screenshots });
-
+  
   let input = 'input.new-todo';
+  await page.goto(`${app.url}`);
+  await page.waitForSelector(input);
 
   for (let i = 0; i < todos; i++) {
     await page.focus(input);
@@ -94,33 +100,84 @@ async function scenarioB(page, app, todos, round = 1, screenshots = false) {
 
   await page.tracing.stop();
 
-  console.log(`Done: Sc#B for ${app.name} with ${todos} todos`);
+  console.log(`#${round}Done: Sc#B for ${app.name} with ${todos} todos`);
 }
 
-async function scenarioC(page, app, todos, round = 1, screenshots = false) {
-  console.log(`\nRunning Sc#C for ${app.name} with ${todos} todos...`);
+async function scenarioC(browser, app, todos, round = 1, screenshots = false) {
+  console.log(`\n#${round} Running Sc#C for ${app.name} with ${todos} todos...`);
+
+  let page = await browser.newPage();
+  await page.goto(`${app.url}?todos=${todos}`);
 
   await page.tracing.start({ path: resultsPath('C', app, todos, round), screenshots });
 
   let todoElements = await page.$$("ul.todo-list li");
 
-  for (let [i, todoElement] of todoElements.entries()) {
+  for (let todoElement of todoElements) {
     await todoElement.click();
     let input = await todoElement.$("input.edit");
     await input.focus();
     await input.type(' updated');
     await page.keyboard.press('Enter');
+    await page.waitForTimeout(250);
+  }
+  await page.waitForTimeout(250);
+  await page.tracing.stop();
 
+  console.log(`#${round} Done: Sc#C for ${app.name} with ${todos} todos`);
+}
+
+async function scenarioD(browser, app, todos, round = 1, screenshots = false) {
+  console.log(`\n#${round} Running Sc#D for ${app.name} with ${todos} todos...`);
+
+  let page = await browser.newPage();
+  await page.goto(`${app.url}?todos=${todos}`);
+  await page.waitForSelector('input.new-todo');
+
+  // Toggle
+  let todoElements = await page.$$(".todo-list li");
+  for (let [i, todoElement] of todoElements.entries()) {
     if (i % 3 === 0) {
-      let toggle = await todoElement.$('.view input');
-      console.log(toggle);
-      await toggle.click();
+      await page.evaluate((i) => {
+        let a = document.querySelector(`.todo-list li:nth-child(${i+1}) input.toggle`);
+        if (a) a.click();
+      }, i);
+      await page.waitForTimeout(250);
     }
   }
 
+  // Filter
+  await page.tracing.start({ path: resultsPath('D', app, todos, round), screenshots });
+
+  await page.click('.filters li:nth-child(2)'); // Active
+  await page.waitForTimeout(250);
+
+  await page.click('.filters li:nth-child(3)'); // Completed
+  await page.waitForTimeout(250);
+
   await page.tracing.stop();
 
-  console.log(`Done: Sc#C for ${app.name} with ${todos} todos`);
+  console.log(`#${round} Done: Sc#D for ${app.name} with ${todos} todos`);
+}
+
+async function scenarioE(browser, app, todos, round = 1, screenshots = false) {
+  console.log(`\n#${round} Running Sc#E for ${app.name} with ${todos} todos...`);
+
+  let page = await browser.newPage();
+  await page.goto(`${app.url}?todos=${todos}`);
+
+  await page.tracing.start({ path: resultsPath('E', app, todos, round), screenshots });
+
+  for (let i = 0; i < todos; i++) {
+    await page.evaluate(() => {
+      let destroyTodo = document.querySelector(".todo-list li:last-child button.destroy");
+      if (destroyTodo) destroyTodo.click();
+    });
+    await page.waitForTimeout(50);
+  }
+  await page.tracing.stop();
+
+  console.log(`#${round} Done: Sc#E for ${app.name} with ${todos} todos`);
 }
 
 main();
